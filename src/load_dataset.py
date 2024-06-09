cbded790fdd554ec6ff3104f51017a3f74cdf8a7
Siddharth Rao <siddrao.926@gmail.com>
2024-06-08 19:10:32 -0700

    Fixed Load Dataset Script

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -8,27 +8,30 @@ def _load_data():
 
 
 def _preprocess_images(train_images, test_images):
-    _normalize_images(train_images, test_images)
-    _reshape_images(train_images, test_images)
+    train_images = _normalize_images(train_images)
+    test_images = _normalize_images(test_images)
+    train_images = _reshape_images(train_images)
+    test_images = _reshape_images(test_images)
+    return train_images, test_images
 
 
-def _normalize_images(train_images, test_images):
-    train_images = train_images.astype('float32') / 255
-    test_images = test_images.astype('float32') / 255
+def _normalize_images(images):
+    images = images.astype('float32') / 255
+    return images
 
 
-def _reshape_images(train_images, test_images):
-    train_images = train_images.reshape((train_images.shape[0], 28, 28, 1))
-    test_images = test_images.reshape((test_images.shape[0], 28, 28, 1))
+def _reshape_images(images):
+    return images.reshape((images.shape[0], 28, 28, 1))
 
 
 def _encode_labels(train_labels, test_labels):
     train_labels = utils.to_categorical(train_labels)
     test_labels = utils.to_categorical(test_labels)
+    return train_labels, test_labels
 
 
 def get_mnist():
     (train_images, train_labels), (test_images, test_labels) = _load_data()
-    _preprocess_images(train_images, test_images)
-    _encode_labels(train_labels, test_labels)
+    train_images, test_images = _preprocess_images(train_images, test_images)
+    train_labels, test_labels = _encode_labels(train_labels, test_labels)
     return (train_images, train_labels), (test_images, test_labels)
