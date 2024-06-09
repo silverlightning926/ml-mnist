fe72bcc2ec0ccf66dc0c7c3466396f2c86430884
Siddharth Rao <siddrao.926@gmail.com>
2024-06-08 20:05:04 -0700

    Fixed Model Loading

--- src/build_model.py
+++ src/build_model.py
@@ -1,4 +1,4 @@
-from keras.api.models import Sequential
+from keras.api.models import Sequential, load_model
 from keras.api.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
 from keras.api.optimizers import RMSprop
 from keras.api.losses import categorical_crossentropy
@@ -8,25 +8,26 @@ import os
 
 
 def _build_model():
+    if (os.path.exists('model.h5')):
+        model = ('model.keras')
 
-    if os.path.exists('./model.keras'):
-        return Sequential().load('model.keras')
+    else:
 
-    model = Sequential([
-        Input(shape=(28, 28, 1)),
-        Conv2D(32, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
-        Flatten(),
-        Dense(64, activation='relu'),
-        Dense(10, activation='softmax')
-    ])
+        model = Sequential([
+            Input(shape=(28, 28, 1)),
+            Conv2D(32, (3, 3), activation='relu'),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3), activation='relu'),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3), activation='relu'),
+            Flatten(),
+            Dense(64, activation='relu'),
+            Dense(10, activation='softmax')
+        ])
 
-    model.compile(optimizer=RMSprop(),
-                  loss=categorical_crossentropy,
-                  metrics=[Accuracy()])
+        model.compile(optimizer=RMSprop(),
+                      loss=categorical_crossentropy,
+                      metrics=[Accuracy()])
 
     return model
 
