ea2273c9c8a41fa2bf8fe6b5cc6ba3fcb904a813
Siddharth Rao <siddrao.926@gmail.com>
2024-06-08 18:51:29 -0700

    Script To Build The Model

---
+++ src/build_model.py
@@ -0,0 +1,29 @@
+import load_dataset
+from keras.api.models import Sequential
+from keras.api.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+
+def _build_model():
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+
+    _compile_model(model)
+
+    return model
+
+
+def _compile_model(model):
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+
+def get_model():
+    return _build_model()
